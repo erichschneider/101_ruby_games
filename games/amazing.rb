1739a9a4c966fd603a7ada9d54764bc7e4ef27a9
Erich Schneider <erich@benefitter.com>
2013-08-23 22:26:35 -0700

    Functioning maze generator!

--- games/amazing.rb
+++ games/amazing.rb
@@ -3,8 +3,6 @@
 # Produce a maze of arbitrary size with one guaranteed path through.
 # Original author: Jack Hauber, Windsor, CT
 
-$/ = nil
-
 def print_header
   print " "*26 + "AMAZING PROGRAM\n"
   print " "*15 + "CREATIVE COMPUTING  MORRISTOWN, NEW JERSEY\n"
@@ -16,8 +14,8 @@ def get_dimensions
   valid = false
   begin
     print("WHAT ARE YOUR WIDTH AND LENGTH? ")
-    dim_str = gets
-    x,y = dim_str.split(',')
+    dim_str = gets.chomp
+    x,y = dim_str.split(',').map { |s| s.to_i }
     if (x > 1 and y > 1)
       valid = true
     else
@@ -25,7 +23,7 @@ def get_dimensions
     end
   end while not valid
   4.times { print "\n" }
-  return [x,y]
+  return x,y
 end
 
 # maze states:
@@ -35,30 +33,30 @@ end
 # 3 - right wall open, floor open
 
 class Maze
-  @height = 0 
-  @width = 0 
-  @start_x = 0
-  @state = []
-  @seen = []
-  @x = 0
-  @y = 0
-  @count = 0
-  @can_exit = false  # "q"
-  @exit_found = false # "z"
+  def initialize(width, height)
+    @height = height
+    @width = width
+    @start_x = 0
+    @state = []
+    @visited = []
+    @x = 0
+    @y = 0
+    @can_exit = false
+    @exit_found = false
 
-  def initialize(height, width)
-    height.times { @seen << width.times.map { 0 } }
-    height.times { @state << width.times.map { 0 } }
+    @height.times { @visited << @width.times.map { 0 } }
+    @height.times { @state << @width.times.map { 0 } }
     @x = @start_x = rand(width)
     @count = 1
-    @seen[0][@x] = @count
+    @visited[0][@x] = @count
     @count += 1
 
     while not done?
-      if not at_left? and unseen_left?
-        if not at_top? and unseen_above?
-          if not at_right? and unseen_right?
-            case rand[3]
+      @stuck = false
+      if not at_left? and unvisited_left?
+        if not at_top? and unvisited_above?
+          if not at_right? and unvisited_right?
+            case rand(3)
             when 0
               open_and_move_left()
             when 1
@@ -68,7 +66,7 @@ class Maze
             end
           else   # can't go right, can go left and up
             if can_go_down?
-              case rand[3]   # line 350
+              case rand(3)
               when 0
                 open_and_move_left()
               when 1
@@ -77,7 +75,7 @@ class Maze
                 open_and_move_down()
               end
             else
-              case rand[2]
+              case rand(2)
               when 0
                 open_and_move_left()
               when 1
@@ -86,9 +84,9 @@ class Maze
             end
           end
         else # can't go up, can go left
-          if not at_right? and unseen_right?
+          if not at_right? and unvisited_right?
             if can_go_down?
-              case rand[3]
+              case rand(3)
               when 0
                 open_and_move_left()
               when 1
@@ -97,7 +95,7 @@ class Maze
                 open_and_move_down()
               end
             else
-              case rand[2]
+              case rand(2)
               when 0
                 open_and_move_left()
               when 1
@@ -106,7 +104,7 @@ class Maze
             end
           else # can't go up or right, can go left
             if can_go_down?
-              case rand[2]
+              case rand(2)
               when 0
                 open_and_move_left()
               when 1
@@ -118,10 +116,10 @@ class Maze
           end
         end
       else   # can't go left
-        if not at_top? and unseen_above?
-          if not at_right? and unseen_right?
+        if not at_top? and unvisited_above?
+          if not at_right? and unvisited_right?
             if can_go_down?
-              case rand[3]
+              case rand(3)
               when 0
                 open_and_move_up()
               when 1
@@ -130,7 +128,7 @@ class Maze
                 open_and_move_down()
               end
             else # can't go left or down
-              case rand[2]
+              case rand(2)
               when 0
                 open_and_move_up()
               when 1
@@ -150,16 +148,43 @@ class Maze
             end
           end
         else  # can't go left or up
-          if not at_right? and unseen_right?
+          if not at_right? and unvisited_right?
+            if can_go_down?
+              case rand(2)
+              when 0
+                open_and_move_right()
+              when 1
+                open_and_move_down()
+              end
+            else 
+              open_and_move_right()
+            end
+          else
             if can_go_down?
               open_and_move_down()
             else
-          
-          
-                
-              
-              
+              @stuck = true
+            end
+          end
+        end
+      end
+      if @stuck
+        # raster scan for a spot we've visited already
+        begin
+          @x, @y = raster_advance(@x, @y)
+        end while @visited[@y][@x] == 0
+      end
+    end
+  end
 
+  def raster_advance(x, y)
+    if x == @width - 1
+      x = 0
+      y = y == @height-1 ? 0 : y+1
+    else
+      x += 1
+    end
+    return x,y
   end
 
   def can_go_down?
@@ -171,53 +196,27 @@ class Maze
         return false
       end
     else
-      return unseen_below?
+      return unvisited_below?
     end
   end
 
   def mark_current
-    @seen[@y][@x] = @count
+    @visited[@y][@x] = @count
     @count += 1
   end
 
-  def done?
-    @count > @height * @width
-  end
-
-  def at_top?
-    @y == 0
-  end
-
-  def at_bottom?
-    @y == @height-1
-  end
-
-  def at_left?
-    @x == 0
-  end
-
-  def at_right?
-    @x == @width - 1
-  end
-
-  def unseen_above?
-    @seen[@y-1][@x] == 0
-  end
-
-  def unseen_below?
-    @seen[@y+1][@x] == 0
-  end
-
-  def unseen_left?
-    @seen[@y][@x-1] == 0
-  end
-
-  def unseen_right?
-    @seen[@y][@x+1] == 0
-  end
+  def done? ; @count > @height * @width ; end
+  def at_top? ; @y == 0 ; end
+  def at_bottom? ; @y == @height-1 ; end
+  def at_left? ; @x == 0 ; end
+  def at_right? ; @x == @width - 1 ; end
+  def unvisited_above? ; @visited[@y-1][@x] == 0 ; end
+  def unvisited_below? ; @visited[@y+1][@x] == 0 ; end
+  def unvisited_left? ; @visited[@y][@x-1] == 0 ; end
+  def unvisited_right? ; @visited[@y][@x+1] == 0 ; end
 
   def open_and_move_right()
-    @state[@y][@x] += 2
+    @state[@y][@x] = @state[@y][@x] == 0 ? 2 : 3
     @x += 1
     mark_current()
   end
@@ -231,8 +230,14 @@ class Maze
 
   def open_and_move_down()
     @state[@y][@x] = @state[@y][@x] == 0 ? 1 : 3
-    @y += 1
-    mark_current()
+    if @can_exit
+      @exit_found = true
+      @can_exit = false
+      @stuck = true
+    else
+      @y += 1
+      mark_current()
+    end
   end
 
   def open_and_move_up()
@@ -243,7 +248,7 @@ class Maze
   end
 
   def print_maze
-    print @width.times.map { |i| i == @start_x ? ".--" : ".  " } + "\n"
+    print @width.times.map { |i| i != @start_x ? ".--" : ".  " }.join("") + ".\n"
     @state.each do |row|
       print "I"
       row.each { |cell| print "  " + (cell < 2 ? "I" : " ") }
@@ -254,20 +259,5 @@ class Maze
   end
 end
 
-
-
 print_header()
-width, height = get_dimensions()
-
-# initialize the "seen" and "maze" arrays to their default states
-seen = []   # we fill this up with numbers as we process each cell
-height.times { seen << width.times.map { 0 } }
-
-maze = []
-height.times { maze << width.times.map { 0 } }
-
-q = 0   # q is set when a possible path through the maze to the bottom found
-z = 0   # z is set when a path through the maze has been established already
-# x is "r" in orig
-# y is "s" in orig
-
+Maze.new(*(get_dimensions())).print_maze()
