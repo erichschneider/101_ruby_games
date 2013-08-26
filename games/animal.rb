9d0f140c598e62ea84f2016ab44059a4614382b2
Erich Schneider <erich@benefitter.com>
2013-08-25 19:23:06 -0700

    Use a more naturalistic implementation of a binary tree. Add notes.

--- games/animal.rb
+++ games/animal.rb
@@ -7,6 +7,115 @@
 # Nathan Teichholtz, DEC
 # Steve North, Creative Computing
 
+class AnimalDatabase
+  def initialize
+    @root = Question.new("DOES IT SWIM")
+    @root.add_yes_answer(Animal.new("FISH"))
+    @root.add_no_answer(Animal.new("BIRD"))
+  end
+
+  def list_animals
+    puts "ANIMALS I ALREADY KNOW ARE:"
+    
+    out_str = ""
+    @root.list_array.each_with_index do |a, i|
+      out_str += sprintf("%-12s", a)
+      out_str += "\n" if i % 5 == 4 
+    end
+    puts out_str
+  end
+  
+  def guess_animal
+    print "ARE YOU THINKING OF AN ANIMAL? "
+    answer = gets.chomp
+    if answer == "LIST"
+      list_animals
+    elsif answer =~ /^y/i
+      node = @root
+      begin
+        node = node.ask_question
+        if not node.is_question?
+          print "IS IT A #{node.text}? "
+          answer = gets
+          if answer =~ /y/i
+            puts "WHY NOT TRY ANOTHER ANIMAL?"
+          else
+            print "THE ANIMAL YOU WERE THINKING OF WAS A ? "
+            new_animal = gets.chomp.upcase
+            puts "PLEASE TYPE IN A QUESTION THAT WOULD DISTINGUISH A "
+            puts "#{new_animal} FROM A #{node.text}"
+            print "? "
+            new_question = gets.chomp.upcase
+            begin
+              puts "FOR A #{new_animal} THE ANSWER WOULD BE "
+              print "? "
+              new_answer = gets[0].upcase
+            end while new_answer != 'Y' and new_answer != 'N'
+            node.replace_with_question(new_question, new_animal, new_answer)
+          end
+        end 
+      end while node.is_question?
+    end
+  end
+end
+
+class Node
+  attr_accessor :parent
+  attr_accessor :yes_node
+  attr_accessor :no_node
+  attr_reader :text
+  def initialize(text)
+    @text = text
+    @yes_node = nil
+    @no_node = nil
+  end
+
+  def is_question? ; self.class == Question ; end
+  def add_yes_answer(a)
+    (@yes_node = a).parent = self
+  end
+
+  def add_no_answer(a)
+    (@no_node = a).parent = self
+  end
+
+  def replace(new_node)
+    parent.send(self == parent.yes_node ? :add_yes_answer : :add_no_answer, 
+                new_node)
+  end
+
+  def list_array
+    ret = [self.list_text]
+    ret << @yes_node.list_array if @yes_node
+    ret << @no_node.list_array if @no_node
+    return ret.flatten.compact
+  end
+end
+
+class Question < Node
+  def ask_question
+    print text + "? "
+    begin
+      if (answer = gets) =~ /^[yn]/
+        return answer[0].upcase == 'Y' ? yes_node : no_node
+      end
+    end while answer !~ /^[yn]/i
+  end
+
+  def list_text; nil; end
+end    
+
+class Animal < Node
+  def replace_with_question(question, new_animal, answer)
+    self.replace(new_q = Question.new(question))
+    new_a = Animal.new(new_animal)
+    new_q.add_yes_answer(answer =~ /y/i ? new_a : self)
+    new_q.add_no_answer(answer =~ /n/i ? new_a : self)
+  end
+
+  def list_text ; text ; end
+end
+    
 def print_header
   puts ' '*32 + "ANIMAL"
   puts ' '*15 + "CREATIVE COMTPUTING  MORRISTOWN, NEW JERSEY"
@@ -16,72 +125,8 @@ def print_header
   puts
 end
 
-data = ["\\QDOES IT SWIM\\Y1\\N2\\",
-        "\\AFISH",
-        "\\ABIRD"]
-
-def ask_question(data, num)
-  data[num] =~ /\\Q(.*)\\Y(.*)\\N(.*)\\/
-  question, yes_branch, no_branch = $1, $2, $3
-  print question + "? "
-  begin
-    answer = gets
-    case answer[0]
-    when 'Y'
-      branch = yes_branch
-    when 'N'
-      branch = no_branch
-    end
-  end while answer[0] != 'Y' and answer[0] != 'N'
-  return Integer(branch)
-end  
-
-def list_data(data)
-  puts "ANIMALS I ALREADY KNOW ARE:"
-  data.each_with_index do |a, i|
-    print $1, ' '*(12-$1.length) if a =~ /\\A(.*)/
-    puts if i % 4 == 0
-  end
-  puts
-end
-
-print_header()
+print_header
+db = AnimalDatabase.new
 while true
-  print "ARE YOU THINKING OF AN ANIMAL? "
-  answer = gets.chomp
-  if answer == "LIST"
-    list_data(data)
-  elsif answer[0]=='Y'
-    k = 0
-    leaf_node = false
-    begin
-      k = ask_question(data, k)
-      if data[k][0..1] != '\\Q'
-        leaf_node = true
-        print "IS IT A #{data[k][2..-1]}? "
-        answer = gets
-        if answer[0] == 'Y'
-          puts "WHY NOT TRY ANOTHER ANIMAL?"
-        else
-          print "THE ANIMAL YOU WERE THINKING OF WAS A ? "
-          new_animal = gets.chomp
-          puts "PLEASE TYPE IN A QUESTION THAT WOULD DISTINGUISH A "
-          puts "#{new_animal} FROM A #{data[k][2..-1]}"
-          print "? "
-          new_question = gets.chomp
-          begin
-            puts "FOR A #{new_animal} THE ANSWER WOULD BE "
-            print "? "
-            new_answer = gets[0]
-          end while new_answer != 'Y' and new_answer != 'N'
-          old_answer = new_answer == 'Y' ? 'N' : 'Y'
-          new_branch = data.length
-          data << data[k]
-          data << "\\A#{new_animal}"
-          data[k] = "\\Q#{new_question}\\#{new_answer}#{new_branch+1}" +
-            "\\#{old_answer}#{new_branch}\\"
-        end
-      end 
-    end while not leaf_node
-  end
+  db.guess_animal
 end
